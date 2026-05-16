c57c0025b0a45df5334c963110888e15de6db177
Rushikesh Shelke <115884339+RushikeshShelke33@users.noreply.github.com>
2026-05-16 13:51:51 +0530

    Add files via upload

---
+++ Gxw2_Ascii_String_To_Hex.py
@@ -0,0 +1,102 @@
+import csv
+
+# ================== INPUT ==================
+text = """SIZE 42.5 mm, 35 mm
+GAP 3 mm, 0 mm
+DIRECTION 0,0
+REFERENCE 0,0
+OFFSET 0 mm
+SET PEEL OFF
+SET CUTTER OFF
+SET PARTIAL_CUTTER OFF
+SET TEAR ON
+CLS
+CODEPAGE 1252
+TEXT 314,254,"ROMAN.TTF",180,1,7,"60% BACK BASE (-) STR--------------"
+TEXT 314,224,"ROMAN.TTF",180,1,6,"PART NO"
+TEXT 232,224,"ROMAN.TTF",180,1,6,"-"
+TEXT 214,224,"ROMAN.TTF",180,1,6,"E2MS46300"
+TEXT 314,195,"ROMAN.TTF",180,1,6,"DATE"
+TEXT 232,195,"ROMAN.TTF",180,1,6,"-"
+TEXT 214,195,"ROMAN.TTF",180,1,6,"09 04 26"
+TEXT 314,166,"ROMAN.TTF",180,1,6,"SR NO"
+TEXT 232,166,"ROMAN.TTF",180,1,6,"-"
+TEXT 214,166,"ROMAN.TTF",180,1,6,"000008"
+TEXT 314,138,"ROMAN.TTF",180,1,6,"CODE"
+TEXT 232,138,"ROMAN.TTF",180,1,6,"-"
+TEXT 214,138,"ROMAN.TTF",180,1,6,"DAE002"
+TEXT 79,223,"ROMAN.TTF",180,1,6,"S202"
+TEXT 110,198,"ROMAN.TTF",180,1,6,"SHIFT"
+TEXT 47,198,"ROMAN.TTF",180,1,6,"A"
+TEXT 114,168,"ROMAN.TTF",180,1,6,"FIREWALL OK"
+BAR 123,119, 3, 106
+QRCODE 216,108,L,3,A,180,M2,S7,"E2MS-46300-0008-090426"
+TEXT 273,39,"ROMAN.TTF",180,1,6,"E2MS-46300-0008-090426"
+PRINT 1,1
+"""
+
+
+def ascii_to_swapped_hex_pairs(text: str):
+    hex_str = text.encode("ascii").hex().upper()
+    bytes_list = [hex_str[i : i + 2] for i in range(0, len(hex_str), 2)]
+
+    pairs = []
+    for i in range(0, len(bytes_list), 2):
+        b1 = bytes_list[i]
+        b2 = bytes_list[i + 1] if i + 1 < len(bytes_list) else "00"
+        pairs.append("H" + b2 + b1)
+
+    return pairs
+
+
+# hex_pairs = ascii_to_swapped_hex_pairs(text)
+# print(hex_pairs)
+
+
+def generate_csv(text, start_register=3000, step_size=1, step_start=0):
+
+    with open("Gxw2PLC_AsciiToHex_F1.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+
+        # Headers
+        writer.writerow(["Step", "LineStarter", "Cmd", "Value"])
+
+        hex_pairs = ascii_to_swapped_hex_pairs(text)
+
+        current_reg = start_register
+        current_step = step_start
+        trackstep = 0
+        # ---- First Row (LD SM400) ----
+        writer.writerow([current_step, "", "LD", "SM400"])
+
+        current_step = 1
+
+        # ---- Data Rows ----
+        for val in hex_pairs:
+
+            # MOV row
+            writer.writerow([current_step, "", "MOV", val])
+
+            # Register row
+            writer.writerow(["", "", "", f"D{current_reg}"])
+
+            trackstep += 2
+            if trackstep != 40:
+
+                current_reg += 1
+                # current_step += step_size + 2
+                current_step += 2
+
+            if trackstep == 40:
+                trackstep = 0
+
+                current_step += 1
+                writer.writerow([current_step, "", "LD", "SM400"])
+                current_step += 1
+        current_step += 1
+        writer.writerow([current_step, "", "END", ""])
+
+    print("✅ CSV generated: Gxw2PLC_AsciiToHex_F1 File Created.csv")
+
+
+generate_csv(text=text, start_register=6000, step_size=2, step_start=0)
